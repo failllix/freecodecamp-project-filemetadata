69f7ffb2bdfbb574856d1a8f71d90599e83c3ac3
Felix Prünte <47022595+failllix@users.noreply.github.com>
2024-11-02 19:57:51 +0100

    Add 'fileanalyse' API endpoint

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var cors = require("cors");
 require("dotenv").config();
+const multer = require("multer");
+const upload = multer({ dest: "./uploads/" });
 
 var app = express();
 
@@ -10,6 +12,20 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
+
+app.post(
+  "/api/fileanalyse",
+  upload.single("upfile"),
+  function (req, res, next) {
+    console.log(req.file);
+
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+    });
+  }
+);
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
